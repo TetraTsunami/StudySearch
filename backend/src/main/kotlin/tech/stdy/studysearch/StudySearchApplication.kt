ce5faa507a9393b4d3d4b8821080d4e0e47713d8
SizableShrimp <sizableshrimp@sizableshrimp.me>
2023-11-11 16:11:08 -0600

    Setup initial Spring project based on https://start.spring.io

---
+++ backend/src/main/kotlin/tech/stdy/studysearch/StudySearchApplication.kt
@@ -0,0 +1,11 @@
+package tech.stdy.studysearch
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class StudySearchApplication
+
+fun main(args: Array<String>) {
+	runApplication<StudySearchApplication>(*args)
+}
---
+++ backend/src/test/kotlin/tech/stdy/studysearch/StudySearchApplicationTests.kt
@@ -0,0 +1,13 @@
+package tech.stdy.studysearch
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class StudySearchApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
