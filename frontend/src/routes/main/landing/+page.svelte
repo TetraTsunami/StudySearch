a3e6796bc4967c47e405b4f464b5f92950a7d4d5
spin-ice <tacos12368@tutanota.com>
2023-11-11 19:37:26 -0600

    start

---
+++ frontend/src/routes/main/landing/+page.svelte
@@ -0,0 +1,17 @@
+<div class="centered-container">
+    <h1 class="page-title">Welcome</h1>
+</div>
+<br/>
+
+
+
+<style>
+    .centered-container {
+        display: flex;
+        justify-content: center;
+    }
+
+    .page-title {
+        font-size: 40pt;
+    }
+</style>
