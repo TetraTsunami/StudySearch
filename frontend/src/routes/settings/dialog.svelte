f2683f2f1898539ef5ab876b428ed864b6145431
Tsuni <akamitsunami@gmail.com>
2023-11-12 11:51:46 -0600

    dialog decodes param

--- frontend/src/routes/settings/dialog.svelte
+++ frontend/src/routes/settings/dialog.svelte
@@ -5,6 +5,8 @@
     if (browser) {
         if (window.location.search !== "") {
             query = window.location.search.substring(1).split("=")[1];
+            query = query.replaceAll("+", " ");
+            query = decodeURIComponent(query);
         }
     }
 </script>
