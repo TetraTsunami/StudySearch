604e985bfe6daa8a9b1f213126ab290f2c8768d1
spin-ice <tacos12368@tutanota.com>
2023-11-11 23:42:03 -0600

    yee finish settings bit

--- frontend/src/routes/settings/gen/+page.svelte
+++ frontend/src/routes/settings/gen/+page.svelte
@@ -1,3 +1,15 @@
+<script lang="ts">
+    let ppl_min_val: string = "0";
+    let ppl_max_val: string = "10"
+    let ppl_max_min: string = "1";
+    let ppl_max_max: string = "10";
+    function ppl_min_update() {
+        let minimum: number = Number.parseInt(ppl_min_val);
+        ppl_max_min = (minimum + 1).toString();
+        ppl_max_max = Math.max(minimum + 1, 10).toString();
+    }
+</script>
+
 <div class="centered-container">
     <h1 class="page-title"><a class="same-unvisited" href="/settings">Settings</a> / General</h1>
 </div>
@@ -13,6 +25,17 @@
         <br/>
         <label for="email">Enter your email (for notifications, optional):</label>
         <input type="email" id="email" placeholder="jdoe@example.com" name="email">
+        <br/>
+        <label for="pss">Do you prefer to be in groups where you're all from the same section?</label>
+        <input type="checkbox" checked name="pss">
+        <br/>
+        <label for="ppl-min">Minimum number of people you prefer:</label>
+        <input type="range" step="1" min="0" max="10" bind:value="{ppl_min_val}" name="ppl-min" on:input={ppl_min_update}>
+        <output>{ppl_min_val}</output>
+        <br/>
+        <label for="ppl-max">Maximum number of people you prefer:</label>
+        <input type="range" step="1" min="{ppl_max_min}" max="{ppl_max_max}" bind:value="{ppl_max_val}" name="ppl-max">
+        <output>{ppl_max_val}</output>
         <br/>
         <button type="submit">Submit</button>
     </form>
@@ -48,5 +71,9 @@
 
     a.same-unvisited:visited {
         color: blue;
+    }
+
+    br {
+        margin: 15px;
     }
 </style>
